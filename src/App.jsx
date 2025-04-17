a887b959818fc418f38e2b3a731bc0e5be557e80
Booklover997 <65503002+Booklover997@users.noreply.github.com>
2025-04-16 22:18:47 -0400

    It works????
    kinda works idrk

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,42 @@
-import * as React from "react";
+import {useEffect,useRef} from "react";
 import AWS from "./assets/AWS.svg";
 import GSEC from "./assets/GSEC.svg";
 import GFACT from "./assets/GFACT.svg";
 import Nav from "./Nav.jsx";
+import Bhavin from "./assets/Bhavin.svg";
 import "./styles/App.css";
 export default function Home() {
+  const containerRef = useRef(null);
+  const threshold = 100;
+
+  useEffect(() => {
+    const handleScroll = () => {
+      var scrollTop = window.pageYOffset * 0.85;
+
+
+      if (containerRef.current) {
+        containerRef.current.style.transform = `translateY(calc(-50% + ${window.pageYOffset}px)) rotate(31.33deg) translateX(calc(-${scrollTop}px))`;
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    handleScroll();
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
-      <Nav/>
-      <h1 className="title">Certs</h1>
+      {/* <Nav/> */}
       <div className="content-body">
-        <div class="c-container">
-          <img src={AWS}/>
-          <img src={GSEC}/>
-          <img src={GFACT}/>
+        <div className="c-container" ref={containerRef}>
+          <img className="cert" src={AWS}/>
+          <img className="cert" src={GSEC}/>
+          <img className="cert"src={GFACT}/>
+          <img className="etc"src={Bhavin}/>
       </div>
       </div>
     </>
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,20 +1,18 @@
 import * as React from "react";
-
+import Bhavin from "./assets/Bhavin.png"
 export default function Nav() {
 
   return (
     <>
       <div className="navbar">
-        <img/>
-        <div className="elements">
-          <ul>
+        <img src={Bhavin}/>
+          <ul className="elements">
             <li>Home</li>
             <li>Certs</li>
             <li>Writeups</li>
             <li>Bhavin</li>
             <li>Enemies</li>
           </ul>
-        </div>
       </div>
     </>  
   );

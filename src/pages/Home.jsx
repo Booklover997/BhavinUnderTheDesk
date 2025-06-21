035af4f14ed1a0e7d18858ae783b81736c2e4aad
Booklover997 <65503002+Booklover997@users.noreply.github.com>
2025-06-21 19:44:31 -0400

    Added card animations
    Added card animations that I think look pretty darn good!

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,24 @@ import Discord  from "../assets/Discord_Slider.svg?react";
 import Instagram  from "../assets/Instagram_Icon.svg?react";
 import Facebook  from "../assets/Facebook_Icon.svg?react";
 
+import Coffee from "../assets/Github_Coffee.svg?react";
+import Star from "../assets/Github_Star.svg?react";
+
+import Claw from "../assets/Claw_Card.svg?react";
+import Jimbo from "../assets/Jimbo_Card.svg?react";
+import Spade from "../assets/Spade_Card.svg?react";
+import Stoat from "../assets/Inscryption_Stoat.svg?react";
+
 export default function Home() {
   return <>
+  <div className = "cardContainer">
+    <div className="cards">
+      <Claw className="card"/>
+      <Jimbo className="card"/>
+      <Spade className="card"/>
+      <Stoat className="card"/>
+    </div>
+  </div>
   <div className="followContainer">
       <h1>Drop A Follow!</h1>
       <div className="socialsContainer">
@@ -15,6 +31,13 @@ export default function Home() {
         <Facebook className="social"/>
       </div>
     </div>
+  <div className="coffeeContainer">
+    <Coffee className="coffee"/>
+    <div>
+      <h1>Give me a star to help me buy coffee hehe</h1>
+      <Star/>
+    </div>
+  </div>
 
   </>
   ;

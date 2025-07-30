03c45709fc0d88ca2452921527ebaf6f38399f14
Booklover997 <65503002+Booklover997@users.noreply.github.com>
2025-07-29 22:08:34 -0400

    123
    123

--- src/pages/Enemies.jsx
+++ src/pages/Enemies.jsx
@@ -9,7 +9,6 @@ export default function Enemies() {
           <h1>My Enemies List</h1>
           <Enemy name="Squid Proxy Lovers" reason="Capitalist merger goons who won't answer me on instagram" />
           <Enemy name="C-bass" reason="Part of SPL and sends me terrible instagram reels" />
-          <Enemy name="Vip3r" reason="Part of SPL also kinda of an SPL insider agent. SPLs greatest ally, smiley, stanky." />
           <Enemy name="Bones Author" reason="Wrote the worst webtoon I have ever read" />
       </div>
     </>

7708001c6a18bffcaeef0969b3125e71d562126d
Booklover997 <rishireddy997@gmail.com>
2025-08-03 19:05:31 -0400

    Fixed jitter issue?

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,7 @@ import Stoat from "../assets/Inscryption_Stoat.svg?react";
 import Card from "../components/Card.jsx";
 export default function Home() {
   return <>
-  <h1>I'm a Cybersecurity student who competes in CTF's with 0xf1sh and a gamer/reader. This website is a compilation of my accomplishments and hobbies as well as funny people from my life. If anyone is mentioned on this website it is purely for fun and not in a serious manner.</h1>
+  <h1>I'm a Cybersecurity student who competes in CTF's with 0xf1sh and a gamer/reader. This website is a compilation of my accomplishments and hobbies as well as funny people from my life. If anyone is mentioned on this website it is purely for fun and not in a serious manner, and is a sign that I respect them.</h1>
   <div className = "cardContainer">
           <h1>I Enjoy  Card Games</h1>
     <div className="cards">
